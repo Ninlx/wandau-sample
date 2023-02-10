20bb8b40c2c4884c578b93fe71e82c0f042f01ac
jeff jeff <jeffernmalinao82@gmail.com>
2023-02-10 15:31:38 +0800

    😫

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
-/* ====== accordion ====== */
-let allPanels = document.querySelectorAll(".accordion > dd")
-allPanels.forEach(function (panel) {
-  panel.style.display = "none"
-})
-
-let panels = document.querySelector(".accordion > dt > a")
-if (panels) {
-  panels.addEventListener("click", function () {
-    let panel = this.parentNode.nextElementSibling
-    if (panel.style.display === "block") {
-      panel.style.display = "none"
-    } else {
-      panel.style.display = "block"
-    }
-    return false
-  })
-}
-
 /* ====== hamburger ====== */
 document.querySelector(".hamburger").addEventListener("click", function () {
   document.querySelector(".hamburger").classList.toggle("active")
@@ -30,78 +11,6 @@ document.querySelector(".search-button").addEventListener("click", function () {
   document.querySelector(".section-wrapper").classList.toggle("no-transform")
 })
 
-/* ====== page transition ====== */
-document.querySelector("body a").addEventListener("click", function (e) {
-  var target = document.querySelector(this).attr("target")
-  var fancybox = document.querySelector(this).data("fancybox")
-  var url = this.getAttribute("href")
-  if (target != "_blank" && typeof fancybox == "undefined" && url.indexOf("#") < 0) {
-    e.preventDefault()
-    var url = this.getAttribute("href")
-    if (url.indexOf("#") != -1) {
-      var hash = url.substring(url.indexOf("#"))
-      if (document.querySelector("body " + hash).length != 0) {
-        document.querySelector(".page-transition").classList.remove("active")
-      }
-    } else {
-      document.querySelector(".page-transition").classList.toggle("active")
-      setTimeout(function () {
-        window.location = url
-      }, 1000)
-    }
-  }
-})
-
-/* ====== tab ====== */
-const tabNav = document.querySelector(".tab-nav li")
-if (tabNav) {
-  tabNav.addEventListener("click", function (e) {
-    document.querySelector(".tab-item").hide()
-    document.querySelector(".tab-nav li").classList.remove("active")
-    document.querySelector(this).classList.add("active")
-    var selected_tab = document.querySelector(this).querySelector("a").attr("href")
-    document.querySelector(selected_tab).stop().show()
-    return false
-  })
-}
-
-/* ====== image box carousel ====== */
-var swiper = new Swiper(".image-box-carousel", {
-  slidesPerView: 1,
-  loop: true,
-  spaceBetween: 0,
-  breakpoints: {
-    640: {
-      slidesPerView: 1,
-      spaceBetween: 0,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 60,
-    }
-  }
-})
-
-/* ====== kinetic teks ====== */
-var slidertexts = new Swiper(".kinetic-texts", {
-  spaceBetween: 10,
-  centeredSlides: true,
-  slidesPerView: 1,
-  touchRatio: 0,
-  slideToClickedSlide: false,
-  loop: true,
-  loopedSlides: 1,
-  effect: "fade",
-  navigation: {
-    nextEl: ".button-next",
-    prevEl: ".button-prev",
-  }
-})
-
 /* ====== preloader ====== */
 let settings = {
   progressSize: 320,
@@ -110,12 +19,12 @@ let settings = {
   lineCap: "round",
   preloaderAnimationDuration: 800,
   startDegree: -90,
-  finalDegree: 270,
-};
+  finalDegree: 270
+}
 
 function setAttributes(elem, attrs) {
   for (let key in attrs) {
-    elem.setAttribute(key, attrs[key]);
+    elem.setAttribute(key, attrs[key])
   }
 }
 
@@ -123,7 +32,7 @@ let preloader = document.createElement("div"),
   canvas = document.createElement("canvas"), size
 (function () {
   let width = window.innerWidth,
-    height = window.innerHeight;
+    height = window.innerHeight
   if (width > height) {
     size = Math.min(settings.progressSize, height / 2)
   } else {
@@ -134,15 +43,14 @@ let preloader = document.createElement("div"),
 setAttributes(preloader, {
   class: "preloader",
   id: "preloader",
-  style:
-    "transition: opacity " + settings.preloaderAnimationDuration / 1000 + "s",
+  style: "transition: opacity " + settings.preloaderAnimationDuration / 1000 + "s"
 })
 
 setAttributes(canvas, {
   class: "progress-bar",
   id: "progress-bar",
   width: settings.progressSize,
-  height: settings.progressSize,
+  height: settings.progressSize
 })
 
 preloader = document.getElementById("preloader")
@@ -256,10 +164,3 @@ const locoScroll = new LocomotiveScroll({
 
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update())
 ScrollTrigger.refresh()
-
-/* ====== odometer ====== */
-let odometer = document.querySelectorAll(".odometer")
-odometer.forEach(function (el) {
-  let count = el.getAttribute("data-count")
-  el.innerHTML = count
-})
